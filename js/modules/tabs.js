fc5d530879357844fe13bde72e411368dc41e852
Vladyslav <kpigoodvlad@gmail.com>
2023-03-28 16:33:27 +0300

    Corrected all the bags and mistakes

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -1,9 +1,8 @@
-function tabs() {
-	// Tabs
-    
-	let tabs = document.querySelectorAll('.tabheader__item'),
-		tabsContent = document.querySelectorAll('.tabcontent'),
-		tabsParent = document.querySelector('.tabheader__items');
+function tabs(tabsSelector, tabsContentSelector, tabsPArentSelector, activeClass) {
+
+	let tabs = document.querySelectorAll(tabsSelector),
+		tabsContent = document.querySelectorAll(tabsContentSelector),
+		tabsParent = document.querySelector(tabsPArentSelector);
 
 	function hideTabContent() {
         
@@ -13,14 +12,14 @@ function tabs() {
 		});
 
 		tabs.forEach(item => {
-			item.classList.remove('tabheader__item_active');
+			item.classList.remove(activeClass);
 		});
 	}
 
 	function showTabContent(i = 0) {
 		tabsContent[i].classList.add('show', 'fade');
 		tabsContent[i].classList.remove('hide');
-		tabs[i].classList.add('tabheader__item_active');
+		tabs[i].classList.add(activeClass);
 	}
     
 	hideTabContent();
@@ -28,7 +27,7 @@ function tabs() {
 
 	tabsParent.addEventListener('click', function(event) {
 		const target = event.target;
-		if(target && target.classList.contains('tabheader__item')) {
+		if(target && target.classList.contains(tabsSelector.slice(1))) {
 			tabs.forEach((item, i) => {
 				if (target == item) {
 					hideTabContent();
@@ -39,4 +38,4 @@ function tabs() {
 	});
 }
 
-module.exports = tabs;
+export default tabs;
--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
 'use strict';
 
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import { openModal } from './modules/modal';
+
 window.addEventListener('DOMContentLoaded', () => {
 
-	const tabs = require('./modules/tabs');
-	const modal = require('./modules/modal');
-	const calc = require('./modules/calc');
-	const forms = require('./modules/forms');
-	const slider = require('./modules/slider');
-	const timer = require('./modules/timer');
-	const cards = require('./modules/cards');
+	const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
 
-	tabs();
-	modal();
-	timer();
+	tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+	modal('[data-modal]', '.modal', modalTimerId);
+	timer('.timer', '2023-05-05');
 	cards();
 	calc();
-	forms();
-	slider();
+	forms('form', modalTimerId);
+	slider({
+		container: '.offer__slider',
+		slide: '.offer__slide', 
+		nextArrow: '.offer__slider-next',
+		prevArrow: '.offer__slider-prev',
+		totalCounter: '#total', 
+		currentCounter: '#current', 
+		wpapper: '.offer__slider-wrapper', 
+		field: '.offer__slider-inner'
+	});
 });
 
 //Rest operator
@@ -264,3 +276,21 @@ window.addEventListener('DOMContentLoaded', () => {
 // }()); 
 
 // user.sayHi();
+
+
+//TRY CATCH
+// try {
+// 	console.log('No mistakes');
+// 	console.log(red);
+// 	console.log('result');
+// } catch (error) {
+// 	console.log(error.name);
+// 	console.log(error.message);
+// 	console.log(error.stack);
+// } finally {
+// 	console.log('I did it');
+// }
+
+// console.log('Still normal');
+
+
